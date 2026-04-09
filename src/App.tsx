cbb92180f34b4e33e3e6d4111b18fc8e447bdcb4
hj-devlog <chj971106@gmail.com>
2026-04-09 11:55:32 +0900

    chore: edit comments (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { useTestStore } from './store/useTestStore';
 
 function App() {
-  const { count, increase, decrease } = useTestStore(); // 2단계에서 만든 Zustand 연결!
+  const { count, increase, decrease } = useTestStore();
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-100">
