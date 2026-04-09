9869a750fe145f3be8ec343f8325e5eeb0810adb
hj <m_i_n_imind@naver.com>
2026-04-09 11:26:59 +0900

    chore: install react-query and devtools (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './App';
 import './index.css';
-import App from './App.tsx';
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -13,6 +18,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {/* 개발 모드에서만 보이는 도구창 */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
