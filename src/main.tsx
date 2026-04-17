5bea33ccd5429c72b0f3326dc233ff66fb30ad95
김우진 <kedw158@gmail.com>
2026-04-17 13:47:20 +0900

    feat(support-chat): 챗봇 위젯 통합 및 메인 적용 (#75)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import { ROUTES } from './constants/routes';
@@ -65,7 +64,6 @@ const renderApp = () => {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </React.StrictMode>,
   );
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,11 @@
 import { authHandlers } from '../features/auth/mocks/handlers';
 import { matchingHandlers } from '../features/matching/mocks/handlers';
+import { supportChatHandlers } from '../features/support-chat/mocks/handlers';
 import { surveyHandlers } from '../features/survey/mocks/handlers';
 
 export const handlers = [
   ...authHandlers,
+  ...supportChatHandlers,
   ...surveyHandlers,
   ...matchingHandlers,
 ];
